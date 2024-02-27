b06bc75dfd0682ec379cee78608d76586e026c99
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-02-27 22:21:55 +0700

    Thêm Type cho SearchDto

--- interfaces/search.dto.ts
+++ interfaces/search.dto.ts
@@ -1,18 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
 import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 export class SearchDTO {
   @ApiProperty({ type: Number, example: 0 })
   @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
   readonly offset: number;
 
   @ApiProperty({ type: Number, example: 10 })
   @IsNotEmpty()
+  @Type(() => Number)
   @IsNumber()
   readonly limit: number;
 
   @ApiProperty({ required: false, type: String, example: '' })
   @IsOptional()
+  @Type(() => String)
   @IsString()
   readonly searchModel?: string;
 }
