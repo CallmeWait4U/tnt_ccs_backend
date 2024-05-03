b2d9ad163e4e735c78819a4eed1598b969a419a4
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-05-04 02:27:27 +0700

    add handlers ActivitiesComplaint

--- src/complaint/complaint.module.ts
+++ src/complaint/complaint.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { CreateActivityComplaintHandler } from './application/command-handler/create.activity,complaint.handler';
 import { CreateComplaintHandler } from './application/command-handler/create.complaint.handler';
 import { CreateTypeComplaintHandler } from './application/command-handler/create.typeComplaint.handler';
+import { DeleteActivityComplaintHandler } from './application/command-handler/delete.activity.complaint.handler';
 import { DeleteComplaintHandler } from './application/command-handler/delete.complaint.handler';
 import { DeleteTypeComplaintHandler } from './application/command-handler/delete.typeComplaint.handler';
 import { UpdateStatusComplaintHandler } from './application/command-handler/update.status.complaint.handler';
 import { UpdateTypeComplaintHandler } from './application/command-handler/update.typeComplaint.handler';
+import { GetActivitiesComplaintHandler } from './application/query-handler/get.activities.complaint.handler';
 import { GetComplaintsHandler } from './application/query-handler/get.complaints.handler';
 import { GetSelectorTypeHandler } from './application/query-handler/get.selector.type.handler';
 import { ReadComplaintHandler } from './application/query-handler/read.complaint.handler';
@@ -19,14 +22,17 @@ import { ComplaintController } from './presentation/complaint.controller';
 const application = [
   CreateComplaintHandler,
   CreateTypeComplaintHandler,
+  CreateActivityComplaintHandler,
   DeleteComplaintHandler,
   DeleteTypeComplaintHandler,
+  DeleteActivityComplaintHandler,
   GetComplaintsHandler,
   ReadComplaintHandler,
   GetSelectorTypeHandler,
   ReadTypeComplaintHandler,
   UpdateStatusComplaintHandler,
   UpdateTypeComplaintHandler,
+  GetActivitiesComplaintHandler,
 ];
 
 const infrastructure = [ComplaintRepository, ComplaintQuery, ComplaintFactory];
