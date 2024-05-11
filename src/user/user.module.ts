0151de9ed93e86b1f9bbd351e8ec87a554b1f498
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-05-11 20:56:30 +0700

    add GetHomePageForEmployeeHandler

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { GetHomePageForEmployeeHandler } from './application/query-handler/get.home.page.for.employee.handler';
 import { GetInfoUserHandler } from './application/query-handler/get.info.user.handler';
 import { UserQuery } from './infrastructure/user.query';
 import { UserController } from './presentation/user.controller';
 
-const application = [GetInfoUserHandler];
+const application = [GetInfoUserHandler, GetHomePageForEmployeeHandler];
 
 const infrastructure = [UserQuery];
 
