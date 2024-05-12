e409a7b33008ee32ef060c972a19c074bcdc8a18
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-05-12 12:23:11 +0700

    comment chat gateways

--- src/chat/presentation/chat.gateway.ts
+++ src/chat/presentation/chat.gateway.ts
@@ -1,47 +1,47 @@
-import { Inject } from '@nestjs/common';
-import {
-  ConnectedSocket,
-  SubscribeMessage,
-  WebSocketGateway,
-  WebSocketServer,
-} from '@nestjs/websockets';
-import { RedisImplement } from 'libs/redis.module';
-import { Server, Socket } from 'socket.io';
+// import { Inject } from '@nestjs/common';
+// import {
+//   ConnectedSocket,
+//   SubscribeMessage,
+//   WebSocketGateway,
+//   WebSocketServer,
+// } from '@nestjs/websockets';
+// import { RedisImplement } from 'libs/redis.module';
+// import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(4001, {
-  cors: {
-    origin: '*',
-  },
-})
-export class chatGateway {
-  @Inject()
-  private readonly redisImplement: RedisImplement;
-  @WebSocketServer()
-  server: Server;
-  userOnline: Socket[] = [];
+// @WebSocketGateway({
+//   cors: {
+//     origin: '*',
+//   },
+// })
+// export class chatGateway {
+//   @Inject()
+//   private readonly redisImplement: RedisImplement;
+//   @WebSocketServer()
+//   server: Server;
+//   userOnline: Socket[] = [];
 
-  @SubscribeMessage('connection')
-  public async handleConnection(@ConnectedSocket() client: Socket) {
-    const token = client.handshake.auth?.token;
-    await this.redisImplement.saveSession(token, client.id);
-    this.userOnline.push(client);
-  }
+//   @SubscribeMessage('connection')
+//   public async handleConnection(@ConnectedSocket() client: Socket) {
+//     const token = client.handshake.auth?.token;
+//     await this.redisImplement.saveSession(token, client.id);
+//     this.userOnline.push(client);
+//   }
 
-  @SubscribeMessage('disconnect')
-  public async handleDisconnect(@ConnectedSocket() client: Socket) {
-    const token = client.handshake.auth?.token;
-    await this.redisImplement.deleteSession(token, client.id);
-    const user = this.userOnline.filter((user) => user.id === client.id)[0];
-    const index = this.userOnline.indexOf(user);
-    if (index > -1) this.userOnline.splice(index, 1);
-  }
+//   @SubscribeMessage('disconnect')
+//   public async handleDisconnect(@ConnectedSocket() client: Socket) {
+//     const token = client.handshake.auth?.token;
+//     await this.redisImplement.deleteSession(token, client.id);
+//     const user = this.userOnline.filter((user) => user.id === client.id)[0];
+//     const index = this.userOnline.indexOf(user);
+//     if (index > -1) this.userOnline.splice(index, 1);
+//   }
 
-  public async handleNotification(token: string, message: string) {
-    const sids = await this.redisImplement.getClients(token);
-    console.log(sids.length);
-    sids.forEach((sid) => {
-      const user = this.userOnline.filter((user) => user.id === sid)[0];
-      user.send(message);
-    });
-  }
-}
+//   public async handleNotification(token: string, message: string) {
+//     const sids = await this.redisImplement.getClients(token);
+//     console.log(sids.length);
+//     sids.forEach((sid) => {
+//       const user = this.userOnline.filter((user) => user.id === sid)[0];
+//       user.send(message);
+//     });
+//   }
+// }
