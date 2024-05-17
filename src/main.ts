237ba66624b17efbd913dd3d2d13dfc730ba20ce
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-05-18 00:55:59 +0700

    update port

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
 }
 bootstrap();
