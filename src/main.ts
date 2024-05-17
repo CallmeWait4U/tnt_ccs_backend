dbe19c5c0bdaa1b5bc85ad3d8e6081ac47123780
CallmeWait4U <nhatthanh2002chuyen@gmail.com>
2024-05-18 00:12:26 +0700

    update littile

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-// import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import * as compression from 'compression';
-// import helmet from 'helmet';
+import * as compression from 'compression';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors();
-  // app.use(helmet());
-  // app.use(compression());
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true,
-  //   }),
-  // );
+  app.enableCors();
+  app.use(helmet());
+  app.use(compression());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .addBearerAuth()
@@ -22,6 +22,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3001);
+  await app.listen(3001, '0.0.0.0');
 }
 bootstrap();
